ccc78977f6a178a6e80d77fec9c4ac223bb749bc
Reshav Sahani <reshav.sahani.9@gmail.com>
2022-06-30 01:59:21 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,7 +286,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 800,
       easing: 'ease-in-out',
       once: true,
       mirror: false,
